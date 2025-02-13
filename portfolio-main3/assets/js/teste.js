732501b8595a3d90d0e42d14a696f09c4518ec3d
Kauanj4 <ko3601622@gmail.com>
2025-02-13 11:43:06 -0300

    mensagem de commit

---
+++ portfolio-main3/assets/js/teste.js
@@ -0,0 +1,23 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.carousel-item');
+
+function showSlide(index) {
+  if (index < 0) {
+    currentIndex = slides.length - 1;
+  } else if (index >= slides.length) {
+    currentIndex = 0;
+  } else {
+    currentIndex = index;
+  }
+
+  const offset = -currentIndex * 100;
+  document.querySelector('.carousel').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+  showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+  showSlide(currentIndex - 1);
+}
